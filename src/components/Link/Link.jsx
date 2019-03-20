face584ae0fd7be666636cf367751f667ee2c43c
Savio Fernandes <td.studios@gmail.com>
2019-03-21 02:41:33 +0800

    add a link component

---
+++ src/components/Link/Link.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link as GatsbyLink } from 'gatsby';
+
+// Since DOM elements <a> cannot receive activeClassName,
+// destructure the prop here and pass it only to GatsbyLink
+const Link = ({ children, to, className, activeClassName, ...other }) => {
+    // Tailor the following test to your environment.
+    // This example assumes that any internal link (intended for Gatsby)
+    // will start with exactly one slash, and that anything else is external.
+    const internal = /^\/(?!\/)/.test(to);
+    const file = /.mel$/.test(to);
+
+    const isPartiallyActive = ({ isPartiallyCurrent }) =>
+        isPartiallyCurrent ? { className: `${className} ${activeClassName}` } : { className: `${className}` };
+
+    // Use Gatsby Link for internal links, and <a> for others
+    if (internal && !file) {
+        return (
+          <GatsbyLink to={to} getProps={isPartiallyActive} {...other}>
+            {children}
+          </GatsbyLink>
+        );
+    }
+    return (
+      <a href={to} className={className} {...other}>
+        {children}
+      </a>
+    );
+};
+
+export default Link;
