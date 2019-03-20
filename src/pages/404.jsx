a2054067ff6834e83d2efa351aafb2a9dda4b73b
Savio Fernandes <td.studios@gmail.com>
2019-03-21 01:54:05 +0800

    add 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import { graphql } from 'gatsby';
+import Layout from '../layout';
+import PostListing from '../components/PostListing/PostListing';
+import config from '../../data/SiteConfig';
+
+class Error404 extends React.Component {
+	render() {
+		const { data } = this.props;
+		const postEdges = data.allMdx.edges;
+		return (
+  <Layout>
+    <div className="content container index-container">
+      <Helmet title={config.siteTitle} />
+      <h1>404: Page not found</h1>
+      <div>Sorry, we've misplaced that URL or it's pointing to something that doesn't exist.</div>
+      <div>Try finding it from the posts below.</div>
+      <div className="post-date hide">
+        <p />
+      </div>
+      <PostListing postEdges={postEdges} />
+    </div>
+  </Layout>
+		);
+	}
+}
+
+export default Error404;
+
+/* eslint no-undef: "off" */
+export const pageQuery = graphql`
+	query {
+		allMdx(limit: 2000, sort: { fields: [frontmatter___date], order: DESC }) {
+			edges {
+				node {
+					fields {
+						slug
+						date
+					}
+					excerpt
+					timeToRead
+					frontmatter {
+						title
+						tags
+						date
+						category
+					}
+				}
+			}
+		}
+	}
+`;
