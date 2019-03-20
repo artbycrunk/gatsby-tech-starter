72406d709f876b15249eb18b9e321d6d0df22ae6
Savio Fernandes <td.studios@gmail.com>
2019-03-21 02:38:02 +0800

    update to use disqus-react

--- src/components/Disqus/Disqus.jsx
+++ src/components/Disqus/Disqus.jsx
@@ -1,45 +1,31 @@
 import React, { Component } from 'react';
-import ReactDisqusComments from 'react-disqus-comments';
+import { DiscussionEmbed, CommentCount } from 'disqus-react';
 import config from '../../../data/SiteConfig';
 import common from '../../../data/common';
+import './Disqus.css';
 
 class Disqus extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			toasts: []
-		};
-		this.notifyAboutComment = this.notifyAboutComment.bind(this);
-		this.onSnackbarDismiss = this.onSnackbarDismiss.bind(this);
-	}
-
-	onSnackbarDismiss() {
-		const [, ...toasts] = this.state.toasts;
-		this.setState({ toasts });
-	}
-
-	notifyAboutComment() {
-		const toasts = this.state.toasts.slice();
-		toasts.push({ text: 'New comment available!' });
-		this.setState({ toasts });
-	}
-
 	render() {
 		const { postNode } = this.props;
-		if (!config.disqusShortname) {
+		const { disqusShortname } = config.serviceIDs;
+		if (!disqusShortname) {
 			return null;
 		}
 		const post = postNode.frontmatter;
-		const url = common.postURL(postNode.fields.slug);
+		const postUrl = common.postURL(postNode.fields.slug);
+		const disqusConfig = {
+			url: postUrl,
+			identifier: postNode.fields.slug,
+			title: post.title
+		};
+
 		return (
-  <ReactDisqusComments
-    shortname={config.disqusShortname}
-    identifier={post.title}
-    title={post.title}
-    url={url}
-    category_id={post.category_id}
-    onNewComment={this.notifyAboutComment}
-  />
+  <div>
+    <CommentCount shortname={disqusShortname} config={disqusConfig}>
+      {'Comments'}
+    </CommentCount>
+    <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+  </div>
 		);
 	}
 }
