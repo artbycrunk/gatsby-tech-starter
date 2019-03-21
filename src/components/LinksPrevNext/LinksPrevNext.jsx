03809094515b36e783efbe349129a967baf450f7
Savio Fernandes <td.studios@gmail.com>
2019-03-21 20:50:36 +0800

    update prevnext stylesheet naming

--- src/components/LinksPrevNext/LinksPrevNext.jsx
+++ src/components/LinksPrevNext/LinksPrevNext.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'gatsby';
-import './LinkPrevNext.css';
+import './LinksPrevNext.css';
 
 class LinksPrevNext extends Component {
     render() {
