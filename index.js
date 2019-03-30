3335359cd4f5f550da4af1f17040c707152df9da
Savio Fernandes <td.studios@gmail.com>
2019-03-30 22:43:38 +0800

    add components to package

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+export {default as Layout} from './src/layout';
+export {default as SEO} from './src/components/SEO/SEO';
+export {default as Disqus} from './src/components/Disqus/Disqus';
+export {default as PostListing} from './src/components/PostListing/PostListing';
+export {default as UserInfo} from './src/components/UserInfo/UserInfo';
+export {default as PostTags} from './src/components/PostTags/PostTags';
+export {default as Link} from './src/components/Link/Link';
+export {default as LinksPrevNext} from './src/components/LinksPrevNext/LinksPrevNext';
+export {default as SocialIcon} from './src/components/SocialIcon/SocialIcon';
+export {default as SocialLinks} from './src/components/SocialLinks/SocialLinks';
+export {default as About} from './src/components/About/About';
+
+export {default as common} from './src/tokens/common';
+export {default as config} from './src/tokens/config';
