3e0f12b083981d468e1383ed44acbfe22721bd2c
Savio Fernandes <td.studios@gmail.com>
2019-04-03 01:12:12 +0800

    simplify component to not use list

--- src/components/LinksPrevNext/LinksPrevNext.jsx
+++ src/components/LinksPrevNext/LinksPrevNext.jsx
@@ -1,38 +1,32 @@
 import React, { Component } from 'react';
-import { Link } from 'gatsby';
+import Link from '../Link/Link';
 import './LinksPrevNext.css';
 
 class LinksPrevNext extends Component {
-    render() {
-        const { prev, next } = this.props;
+	render() {
+		const { prev, next } = this.props;
 
-        const links = [];
-
-        if (prev) {
-            links.push(
-              <div className="pnlinks-grid-item-left">
-                <div>{' < Previous'}</div>
-                <Link className="contentLink" key={prev.fields.slug} to={prev.fields.slug}>
-                  {prev.fields.title}
-                </Link>
-              </div>
-            );
-        } else {
-            links.push(<div />);
-        }
-        if (next) {
-            links.push(
-              <div className="pnlinks-grid-item-right">
-                <div>{'Next > '}</div>
-                <Link className="contentLink" key={next.fields.slug} to={next.fields.slug}>
-                  {next.fields.title}
-                </Link>
-              </div>
-            );
-        }
-
-        return <div className="pnlinks-grid-container">{links}</div>;
-    }
+		return (
+  <div className="pnlinks-grid-container">
+    {prev && (
+    <div className="pnlinks-grid-item-left">
+      <div>{' < Previous'}</div>
+      <Link className="contentLink" key={prev.fields.slug} to={prev.fields.slug}>
+        {prev.fields.title}
+      </Link>
+    </div>
+				)}
+    {next && (
+    <div className="pnlinks-grid-item-right">
+      <div>{'Next > '}</div>
+      <Link className="contentLink" key={next.fields.slug} to={next.fields.slug}>
+        {next.fields.title}
+      </Link>
+    </div>
+				)}
+  </div>
+		);
+	}
 }
 
 export default LinksPrevNext;
