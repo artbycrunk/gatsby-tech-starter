18e231830c5bfd668b60a2cf567f3cf493e352b0
Savio Fernandes <td.studios@gmail.com>
2019-04-04 00:59:21 +0800

    better styling for code block with title

--- src/components/Code/Code.jsx
+++ src/components/Code/Code.jsx
@@ -1,38 +1,24 @@
 import React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
-import styled from 'styled-components';
-import theme from 'prism-react-renderer/themes/nightOwl';
+import theme from 'prism-react-renderer/themes/vsDarkPlus';
+import './Code.css';
 
-
-export const Pre = styled.pre`
-	text-align: left;
-	margin: 1em 0;
-	padding: 0.5em;
-`;
-
-export const LineNo = styled.span`
-	display: inline-block;
-	width: 2em;
-	user-select: none;
-	opacity: 0.3;
-`;
-
-const Code = ({ codeString, language, ...props }) => {
+const Code = ({ codeString, language, title, ...props }) => {
 	return (
   <Highlight {...defaultProps} code={codeString} language={language} theme={theme}>
     {({ className, style, tokens, getLineProps, getTokenProps }) => (
-      <div className="gatsby-highlight">
-        <Pre className={className} style={style}>
+      <div className="gatsby-highlight" data-language={language}>
+        <div className="gatsby-code-title">{title}</div>
+        <pre className={className} style={style}>
           {tokens.map((line, i) => (
             <div {...getLineProps({ line, key: i })}>
-              <LineNo>{i + 1}</LineNo>
+              <span className="gatsby-code-lineno">{i + 1}</span>
               {line.map((token, key) => (
                 <span {...getTokenProps({ token, key })} />
 								))}
-
             </div>
 						))}
-        </Pre>
+        </pre>
       </div>
 			)}
   </Highlight>
