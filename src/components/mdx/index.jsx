b628edbe458798b25c7b41d03f888f5ff2efd194
Savio Fernandes <td.studios@gmail.com>
2019-04-04 00:56:03 +0800

    improve preToCodeBlock logic

--- src/components/mdx/index.jsx
+++ src/components/mdx/index.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
-import { preToCodeBlock } from 'mdx-utils';
-
 import Title from './Title';
 import Subtitle from './Subtitle';
 import Paragraph from './Paragraph';
 import Link from '../Link/Link';
 import Code from '../Code/Code';
+
+function preToCodeBlock(preProps) {
+	if (
+		// children is MDXTag
+		preProps.children &&
+		// MDXTag props
+		preProps.children.props &&
+		// if MDXTag is going to render a <code>
+		preProps.children.props.name === 'code'
+	) {
+		// we have a <pre><code> situation
+		const {
+			children: codeString,
+			props: { className, ...props }
+		} = preProps.children.props;
+
+		let language;
+		let title;
+		if (className) {
+			const [_language, _title] = className.split(':');
+			if (_language) [, language] = _language.split('-');
+			if (_title) [, title] = _title.split('=');
+		}
+
+		return {
+			codeString: codeString.trim(),
+			language,
+			title,
+			...props
+		};
+	}
+	return false;
+}
 
 export default {
 	h1: props => <Title {...props} />,
