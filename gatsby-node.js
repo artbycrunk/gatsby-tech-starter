67169026388581da0438553ed86017c1b4196eb1
Savio Fernandes <artbycrunk@gmail.com>
2019-04-16 22:59:00 +0700

    remove unwanted alias comments

--- gatsby-node.js
+++ gatsby-node.js
@@ -134,10 +134,7 @@ exports.setFieldsOnGraphQLNodeType = ({ type, actions }) => {
 exports.onCreateWebpackConfig = ({ actions }) => {
 	actions.setWebpackConfig({
 		resolve: {
-			modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-			// alias: {
-			// 	$components: path.resolve(__dirname, 'src/components')
-			// }
+			modules: [path.resolve(__dirname, 'src'), 'node_modules']
 		}
 	});
 };
