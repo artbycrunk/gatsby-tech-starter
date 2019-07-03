4528418bab6cb702153348ea6512183ede7feb5a
Savio Fernandes <artbycrunk@gmail.com>
2019-07-04 00:24:51 +0700

    change to vsDark theme

--- src/components/Code/Code.jsx
+++ src/components/Code/Code.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
-import theme from 'prism-react-renderer/themes/vsDarkPlus';
+import theme from 'prism-react-renderer/themes/vsDark';
 import './Code.css';
 
 const Code = ({ codeString, language, title, ...props }) => {
